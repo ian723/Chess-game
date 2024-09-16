c2fa6a53d3f6fa733f3c347eaf31330d2a0f46fe
ian723 <ndegwaian241@gmail.com>
2024-09-16 13:40:17 +0300

    message

--- backend1/dist/GameManager.js
+++ backend1/dist/GameManager.js
@@ -1,48 +1,42 @@
-import { WebSocket } from "ws";
-import { INIT_GAME, MOVE } from "./message";
-import { Game } from "./Game";
-
-export class GameManager {
-    private games: Game[];
-    private pendingUser: WebSocket | null;
-    private users: WebSocket[];
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameManager = void 0;
+const message_1 = require("./message");
+const Game_1 = require("./Game");
+class GameManager {
     constructor() {
         this.games = [];
         this.pendingUser = null;
         this.users = [];
     }
-
-    addUser(socket: WebSocket) {
+    addUser(socket) {
         this.users.push(socket);
-        this.addHandler(socket)
+        this.addHandler(socket);
     }
-
-    removeUser(socket: WebSocket) {
+    removeUser(socket) {
         this.users = this.users.filter(user => user !== socket);
         // Stop the game here becausee the user left
     }
-
-    private addHandler(socket: WebSocket) {
+    addHandler(socket) {
         socket.on("message", (data) => {
             const message = JSON.parse(data.toString());
-
-            if (message.type === INIT_GAME) {
+            if (message.type === message_1.INIT_GAME) {
                 if (this.pendingUser) {
-                    const game = new Game(this.pendingUser, socket);
+                    const game = new Game_1.Game(this.pendingUser, socket);
                     this.games.push(game);
                     this.pendingUser = null;
-                } else {
+                }
+                else {
                     this.pendingUser = socket;
                 }
             }
-
-            if (message.type === MOVE) {
+            if (message.type === message_1.MOVE) {
                 const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
                 if (game) {
                     game.makeMove(socket, message.move);
                 }
             }
-        })
+        });
     }
 }
+exports.GameManager = GameManager;
---
+++ backend1/src/message.ts
@@ -0,0 +1,3 @@
+export const INIT_GAME = "init-game";
+export const MOVE = "move";
+export const GAME_OVER = "game_over";
