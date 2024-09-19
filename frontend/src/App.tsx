a1ca387821b08ec46906e78241579199a0532cc2
ian723 <ndegwaian241@gmail.com>
2024-09-19 15:04:16 +0300

    message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import {} from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 import { Landing } from './screens/Landing';
@@ -7,14 +6,14 @@ import { Game } from './screens/Game';
 function App() {
 
   return (
-    <>
+    <div className='h-screen bg-slate-950'>
          <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/game" element={<Game />} />
       </Routes>
     </BrowserRouter>
-    </>
+    </div>
   )
 }
 
--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,5 +1,26 @@
 export const Landing = () => {
-    return <div>
-        Landing page
+  return (
+    <div>
+      <div className="pt-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="flex justify-center">
+            <img src={"/chessboard.jpg"} className="max-w-96" alt="Chess" />
+          </div>
+          <div>
+            <div className="flex justify-center">
+              <h1 className="text-4xl font-bold text-white">
+                Play chess online on the #2 Site
+              </h1>
+            </div>
+
+            <div className="mt-4 flex justify-center">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Play Online
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-}
+  );
+};
